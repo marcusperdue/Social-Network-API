d77e57bd29e41ba28146e5e9d23024132b6c561f
Marcus Perdue <marcusperduee@gmail.com>
2023-11-26 16:33:56 -0800

    added user controller js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
 const { User } = require('../models');
 
 const userController = {
+  // Controller logic for getting all users
   getAllUsers: async (req, res) => {
     try {
-      const users = await User.find().populate('thoughts').populate('friends');
+      const users = await User.find();
       res.json(users);
     } catch (error) {
       console.error(error);
@@ -11,7 +12,64 @@ const userController = {
     }
   },
 
-  // Other controller functions for user routes (e.g., createUser, updateUser, deleteUser, etc.)
+  // Controller logic for getting a user by ID
+  getUserById: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.userId);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json(error);
+    }
+  },
+
+  // Controller logic for creating a new user
+  createUser: async (req, res) => {
+    try {
+      const newUser = await User.create(req.body);
+      res.json(newUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json(error);
+    }
+  },
+
+  // Controller logic for updating a user by ID
+  updateUserById: async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        req.body,
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(updatedUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json(error);
+    }
+  },
+
+  // Controller logic for deleting a user by ID
+  deleteUserById: async (req, res) => {
+    try {
+      const deletedUser = await User.findByIdAndDelete(req.params.userId);
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(deletedUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json(error);
+    }
+  },
+
+  // Add other user-related controllers as needed
 };
 
 module.exports = userController;
