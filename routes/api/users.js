3bbaca6df6c9a2661f8a027f5b101ef5e2d78ad3
Marcus Perdue <marcusperduee@gmail.com>
2023-11-27 17:33:03 -0800

    added crud for user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { User } = require('../../models'); 
+const { User } = require('../../models');
 
+// GET all users
 router.get('/users', async (req, res) => {
   try {
     const users = await User.find().populate('thoughts').populate('friends');
@@ -10,4 +11,70 @@ router.get('/users', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+// GET a single user by _id and populate thought and friend data
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .populate('thoughts')
+      .populate('friends');
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+// POST a new user
+router.post('/users', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+});
+
+// PUT to update a user by _id
+router.put('/users/:id', async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+});
+
+// DELETE a user by _id
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(deletedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
