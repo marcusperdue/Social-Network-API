88cff55f77720ad95f270f1d3a7b1c659cd356da
Marcus Perdue <marcusperduee@gmail.com>
2023-11-27 17:38:34 -0800

    fixed user routes

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const { Thought } = require('../../models');
 
-// Define routes for thoughts
-router.get('/', async (req, res) => {
+router.get('/thoughts', async (req, res) => {
   try {
     const thoughts = await Thought.find();
     res.json(thoughts);
@@ -11,5 +10,94 @@ router.get('/', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+// GET to get a single thought by its _id
+router.get('/thoughts/:thoughtId', async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.thoughtId);
+
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+
+    res.json(thought);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+// POST to create a new thought
+router.post('/thoughts', async (req, res) => {
+  try {
+    const newThought = await Thought.create(req.body);
+
+    // Push the created thought's _id to the associated user's thoughts array field
+    const user = await User.findByIdAndUpdate(
+      req.body.userId,
+      { $push: { thoughts: newThought._id } },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(201).json(newThought);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+});
+
+// PUT to update a thought by its _id
+router.put('/thoughts/:thoughtId', async (req, res) => {
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(
+      req.params.thoughtId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedThought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+
+    res.json(updatedThought);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+});
+
+// DELETE to remove a thought by its _id
+router.delete('/thoughts/:thoughtId', async (req, res) => {
+  try {
+    const deletedThought = await Thought.findByIdAndDelete(req.params.thoughtId);
+
+    if (!deletedThought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+
+    // Remove the thought's _id from the associated user's thoughts array field
+    const user = await User.findByIdAndUpdate(
+      deletedThought.userId,
+      { $pull: { thoughts: req.params.thoughtId } },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(deletedThought);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+  
+});
+
+
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,19 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Error handling middleware
+const handleErrors = (res, error) => {
+  console.error(error);
+  res.status(500).json({ success: false, error: 'Internal server error' });
+};
+
 // GET all users
 router.get('/users', async (req, res) => {
   try {
     const users = await User.find().populate('thoughts').populate('friends');
-    res.json(users);
+    res.json({ success: true, data: users });
   } catch (error) {
-    console.error(error);
-    res.status(500).json(error);
+    handleErrors(res, error);
   }
 });
 
@@ -76,5 +81,56 @@ router.delete('/users/:id', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+
+
+
+
+
+// POST to add a new friend to a user's friend list
+router.post('/users/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friend = await User.findById(req.params.friendId);
+
+    if (!user || !friend) {
+      return res.status(404).json({ message: 'User or friend not found' });
+    }
+
+    // Check if the friend is already in the user's friends list
+    if (user.friends.includes(req.params.friendId)) {
+      return res.status(400).json({ message: 'Friend already added' });
+    }
+
+    user.friends.push(req.params.friendId);
+    await user.save();
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+
+// DELETE to remove a friend from a user's friend list
+router.delete('/users/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    user.friends.pull(req.params.friendId);
+    await user.save();
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
 
 module.exports = router;
